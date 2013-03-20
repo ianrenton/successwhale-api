6432082dfb4e51a9be1a5ab74f7158b275e5c312
Ian Renton <ian@ianrenton.com>
2013-03-20 22:23:15 +0000

    Changed the way the feeds API displays retweets. Hopefully for the better!

--- classes/item.rb
+++ classes/item.rb
@@ -14,44 +14,55 @@ class Item
 
   # Fills in the contents of the item based on a tweet.
   def populateFromTweet (tweet)
-    @content.merge!(:text => tweet.full_text)
-    @content.merge!(:id => tweet.id)
-    @content.merge!(:time => tweet.created_at)
-    @content.merge!(:fromuser => tweet.from_user)
-    @content.merge!(:fromusername => tweet.user.name)
-    @content.merge!(:fromuseravatar => tweet.user.profile_image_url)
-    @content.merge!(:isreply => tweet.reply?)
-    @content.merge!(:isretweet => tweet.retweet?)
-    @content.merge!(:numfavourited => tweet.favoriters_count)
-    @content.merge!(:numretweeted => tweet.retweeters_count)
-    @content.merge!(:numreplied => tweet.repliers_count)
-    @content.merge!(:numfavourited => tweet.favoriters_count)
-    @content.merge!(:inreplytostatusid => tweet.in_reply_to_status_id)
-    @content.merge!(:inreplytouserid => tweet.in_reply_to_user_id)
-    @content.merge!(:urls => tweet.urls)
 
     if tweet.retweet?
-      @content.merge!(:retweet => {})
-      @content[:retweet].merge!(:text => tweet.retweeted_status.full_text)
-      @content[:retweet].merge!(:id => tweet.retweeted_status.id)
-      @content[:retweet].merge!(:time => tweet.retweeted_status.created_at)
-      @content[:retweet].merge!(:fromuser => tweet.retweeted_status.from_user)
-      @content[:retweet].merge!(:fromusername => tweet.retweeted_status.user.name)
-      @content[:retweet].merge!(:fromuseravatar => tweet.retweeted_status.user.profile_image_url)
-      @content[:retweet].merge!(:isreply => tweet.retweeted_status.reply?)
-      @content[:retweet].merge!(:isretweet => tweet.retweeted_status.retweet?)
-      @content[:retweet].merge!(:numfavourited => tweet.retweeted_status.favoriters_count)
-      @content[:retweet].merge!(:numretweeted => tweet.retweeted_status.retweeters_count)
-      @content[:retweet].merge!(:numreplied => tweet.retweeted_status.repliers_count)
-      @content[:retweet].merge!(:numfavourited => tweet.retweeted_status.favoriters_count)
-      @content[:retweet].merge!(:inreplytostatusid => tweet.retweeted_status.in_reply_to_status_id)
-      @content[:retweet].merge!(:inreplytouserid => tweet.retweeted_status.in_reply_to_user_id)
-      @content[:retweet].merge!(:urls => tweet.retweeted_status.urls)
+      # Keep the retweet's ID for replies and the time for sorting
+      @content.merge!(:id => tweet.id)
+      @content.merge!(:time => tweet.created_at)
+
+      # Add extra tags to show who retweeted it and when
+      @content.merge!(:retweetedbyuser => tweet.from_user)
+      @content.merge!(:retweetedat => tweet.created_at)
+      @content.merge!(:isretweet => tweet.retweet?)
+
+      # Copy the retweeted status's data into the main body
+      @content.merge!(:text => tweet.retweeted_status.full_text)
+      @content.merge!(:fromuser => tweet.retweeted_status.from_user)
+      @content.merge!(:fromusername => tweet.retweeted_status.user.name)
+      @content.merge!(:fromuseravatar => tweet.retweeted_status.user.profile_image_url)
+      @content.merge!(:isreply => tweet.retweeted_status.reply?)
+      @content.merge!(:numfavourited => tweet.retweeted_status.favoriters_count)
+      @content.merge!(:numretweeted => tweet.retweeted_status.retweeters_count)
+      @content.merge!(:numreplied => tweet.retweeted_status.repliers_count)
+      @content.merge!(:numfavourited => tweet.retweeted_status.favoriters_count)
+      @content.merge!(:inreplytostatusid => tweet.retweeted_status.in_reply_to_status_id)
+      @content.merge!(:inreplytouserid => tweet.retweeted_status.in_reply_to_user_id)
+      @content.merge!(:urls => tweet.retweeted_status.urls)
+
+    else
+      # Not a retweet, so populate the content of the item normally.
+      @content.merge!(:text => tweet.full_text)
+      @content.merge!(:id => tweet.id)
+      @content.merge!(:time => tweet.created_at)
+      @content.merge!(:fromuser => tweet.from_user)
+      @content.merge!(:fromusername => tweet.user.name)
+      @content.merge!(:fromuseravatar => tweet.user.profile_image_url)
+      @content.merge!(:isreply => tweet.reply?)
+      @content.merge!(:isretweet => tweet.retweet?)
+      @content.merge!(:numfavourited => tweet.favoriters_count)
+      @content.merge!(:numretweeted => tweet.retweeters_count)
+      @content.merge!(:numreplied => tweet.repliers_count)
+      @content.merge!(:numfavourited => tweet.favoriters_count)
+      @content.merge!(:inreplytostatusid => tweet.in_reply_to_status_id)
+      @content.merge!(:inreplytouserid => tweet.in_reply_to_user_id)
+      @content.merge!(:urls => tweet.urls)
     end
 
     # TODO: fill in actions
-    # TODO: Pick out URLs like Twitter
   end
+
+  # TODO: Populate from Facebook
+  # TODO: Populate from LinkedIn
 
   # Returns the item as a hash.
   def asHash
