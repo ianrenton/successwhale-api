336281159f7502621366e8eaf6f3c7ed164ab467
Ian Renton <ian@ianrenton.com>
2013-04-19 18:17:06 +0100

    Testing Facebook auth

--- apifuncs/v3/authwithfacebook.rb
+++ apifuncs/v3/authwithfacebook.rb
@@ -33,7 +33,7 @@ get '/v3/authwithfacebook.?:format?' do
       end
     else
       # A code was returned, so let's validate it
-      token = @facebookOAuth.get_access_token(params[:code], {:redirect_uri => request.url})
+      token = @facebookOAuth.get_access_token(params[:code], {:redirect_uri => "#{request.base_url}#{request.path_info}"})
       # Get FB userid to add to DB
       facebookClient = Koala::Facebook::API.new(token)
       fb_uid = facebookClient.get_object("me")[:id]
